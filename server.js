09be4ee6e669b2ef7e24e910f8e90b2153f3ddcb
Lukas <lukas.hass.lukas@gmail.com>
2018-02-21 16:49:45 +0100

    implemented timer

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const WebSocket = require('ws');
-const mysql = require('mysql');
-const cmd = require('node-cmd');
-const CONFIG = require('./config.json');
+const WebSocket = require('ws')
+const mysql = require('mysql')
+const cmd = require('node-cmd')
+const schedule = require('node-schedule')
+const CONFIG = require('./config.json')
 const mysqlCon = {
         host     : CONFIG.dbHost,
         user     : CONFIG.dbUser,
@@ -9,17 +10,27 @@ const mysqlCon = {
         database : CONFIG.dbName
 }
 
-var db = mysql.createConnection(mysqlCon);
+var db = mysql.createConnection(mysqlCon)
 
-const wss = new WebSocket.Server({ port: CONFIG.wsPort });
+const wss = new WebSocket.Server({ port: CONFIG.wsPort })
 
-var sockets;
+var sockets
+var timers
+var jobs = []
 
 db.query('SELECT * FROM sockets', function (error, results, fields) {
-	if (error) throw error;
+	if (error) throw error
 
-	sockets = results;
+	sockets = results
 
+});
+
+db.query('SELECT * FROM timers', function (error, results, fields) {
+	if (error) throw error
+
+	timers = results
+
+    initJobs()
 });
 
 wss.on('connection', function connection(ws) {
@@ -34,7 +45,7 @@ wss.on('connection', function connection(ws) {
 
 	ws.on('pong', () => {
       		ws.isAlive = true
-    	})
+    })
 });
 
 // WebSocket keep alive pings
@@ -80,7 +91,8 @@ function incoming(ws, message) {
 
 	switch (data.type) {
 		case 'sockets':
-			updateSockets(ws, data.sockets)
+			updateSockets(data.sockets)
+            broadcastOthers(ws)
 			break
 	}
 }
@@ -91,34 +103,57 @@ function sendConnection (ws, data) {
 	}
 }
 
-function updateSockets(ws, data){
+function updateSockets(data){
 	var old = sockets
 	sockets = data
-	if(data.length == old.length) { // test not really good enough
+	if(sockets.length == old.length) { // test not really good enough
 
-		data.forEach( function(item, i) {
-			// data[i] === item
-			if(data[i] != old[i]) {
+		sockets.forEach( function(item, i) {
+			// sockets[i] === item
+			if(sockets[i] != old[i]) {
 
 				db.query('UPDATE sockets SET status = ? WHERE id = ?', [item.status, item.id], function (error, results, fields) {
 					if (error) throw error
 				})
 
-				if(data[i].status != old[i].status) {
+				if(sockets[i].status != old[i].status) {
 					cmd.run("sudo ./codesendRoot " + (item.status == 1 ? parseInt(item.code_on, 10) : parseInt(item.code_off, 10)) + " " + parseInt(item.protocol, 10))
 				}
 			}
 		})
 	}
+}
 
-	// process new data
+function broadcastOthers(ws) {
+    wss.clients.forEach( function(client) {
+        if(client !== ws && client.readyState === WebSocket.OPEN){
+            sendConnection(client, {
+                'type': 'sockets',
+                'sockets': sockets
+            })
+        }
+    })
+}
 
-	wss.clients.forEach( function(client) {
-		if(client !== ws){
-			sendConnection(client, {
-				'type': 'sockets',
-				'sockets': sockets
-			})
-		}
-	})
+function broadcastAll() {
+    wss.clients.forEach( function(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            sendConnection(client, {
+                'type': 'sockets',
+                'sockets': sockets
+            })
+        }
+    })
+}
+
+function initJobs() {
+    timers.forEach( function(timer) {
+        jobs[timer.id] = schedule.scheduleJob(timer.minute + ' ' + timer.hour + ' * * *', function(){
+            var data = sockets
+            data[timer.socket_id].status = timer.action
+            updateSockets(data)
+            broadcastAll()
+            console.log(timer.socket_id + ' ' + timer.action);
+        })
+    })
 }
