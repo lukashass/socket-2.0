630e6d6cafb200564922698349b2575f2a128474
Lukas Hass <lukas.hass.lukas@gmail.com>
2018-03-11 19:40:17 +0100

    no longer using array index for socket id, beautify

--- htdocs/app.js
+++ htdocs/app.js
@@ -1,234 +1,304 @@
 Vue.config.devtools = true
 
 var vm = new Vue({
-	el: '#app',
-	data: {
-        // UI
-		drawer: null,
-        view: 'socket',
-        dialog: false,
+  el: '#app',
+  data: {
+    // UI
+    drawer: null,
+    view: 'socket',
+    dialog: false,
 
-        // websocket
-		connected: false,
-		auth: false,
-        password: '',
-        pwVis: true,
+    // websocket
+    connected: false,
+    auth: false,
+    password: '',
+    pwVis: true,
 
-        // backend
-		sockets: [],
-        headers: [
-            {
-              text: 'Socket name',
-              align: 'left',
-              sortable: false,
-              value: 'name'
-            },
-            { text: 'Action', value: 'action' },
-            { text: 'Mode', value: 'mode' },
-            { text: 'Offset', value: 'offset' },
-            { text: 'Minute', value: 'minute' },
-            { text: 'Hour', value: 'hour' },
-            { text: 'Day of Month', value: 'dom' },
-            { text: 'Month', value: 'month' },
-            { text: 'Day of Week', value: 'dow' },
-            { text: 'Actions', value: 'name', sortable: false }
-        ],
-        timers: [],
+    // backend
+    sockets: [],
+    headers: [{
+      text: 'Socket name',
+      align: 'left',
+      sortable: false,
+      value: 'name'
+    },
+    {
+      text: 'Action',
+      value: 'action'
+    },
+    {
+      text: 'Mode',
+      value: 'mode'
+    },
+    {
+      text: 'Offset',
+      value: 'offset'
+    },
+    {
+      text: 'Minute',
+      value: 'minute'
+    },
+    {
+      text: 'Hour',
+      value: 'hour'
+    },
+    {
+      text: 'Day of Month',
+      value: 'dom'
+    },
+    {
+      text: 'Month',
+      value: 'month'
+    },
+    {
+      text: 'Day of Week',
+      value: 'dow'
+    },
+    {
+      text: 'Actions',
+      value: 'name',
+      sortable: false
+    }
+    ],
+    timers: [],
 
-        // timer dialog
-        editedIndex: -1,
-        editedTimer: {
-            socket_id: 0,
-            action: 0,
-            mode: 'time',
-            offset: 0,
-            minute: '*',
-            hour: '*',
-            dom: '*',
-            month: '*',
-            dow: '*'
-        },
-        defaultTimer: {
-            socket_id: 0,
-            action: 0,
-            mode: 'time',
-            offset: 0,
-            minute: '*',
-            hour: '*',
-            dom: '*',
-            month: '*',
-            dow: '*'
-        },
-        modes: [
-            {text: 'Time', value: 'time'},
-            {text: 'Dawn', value: 'dawn'},
-            {text: 'Sunrise', value: 'sunrise'},
-            {text: 'Noon', value: 'solarNoon'},
-            {text: 'Sunset', value: 'sunset'},
-            {text: 'Dusk', value: 'dusk'}
-        ],
-        actions: [
-            {text: 'On', value: 1},
-            {text: 'Off', value: 0}
-        ]
+    // timer dialog
+    editedIndex: -1,
+    editedTimer: {
+      socket_id: 0,
+      action: 0,
+      mode: 'time',
+      offset: 0,
+      minute: '*',
+      hour: '*',
+      dom: '*',
+      month: '*',
+      dow: '*'
+    },
+    defaultTimer: {
+      socket_id: 0,
+      action: 0,
+      mode: 'time',
+      offset: 0,
+      minute: '*',
+      hour: '*',
+      dom: '*',
+      month: '*',
+      dow: '*'
+    },
+    modes: [{
+      text: 'Time',
+      value: 'time'
+    },
+    {
+      text: 'Dawn',
+      value: 'dawn'
+    },
+    {
+      text: 'Sunrise',
+      value: 'sunrise'
+    },
+    {
+      text: 'Noon',
+      value: 'solarNoon'
+    },
+    {
+      text: 'Sunset',
+      value: 'sunset'
+    },
+    {
+      text: 'Dusk',
+      value: 'dusk'
+    }
+    ],
+    actions: [{
+      text: 'On',
+      value: 1
+    },
+    {
+      text: 'Off',
+      value: 0
+    }
+    ]
 
-	},
+  },
 
-	methods: {
-        send: function(raw) {
-            ws.send(JSON.stringify(raw))
-        },
-		power: function(id, action) {
-			this.sockets.forEach( function(socket) {
-				if(socket.id == id) {
-					socket.status = action
-				}
-			})
-			var raw = {'type': 'sockets', 'sockets': this.sockets}
-			this.send(raw)
-		},
-        setView: function(view) {
-            this.view = view
-            this.drawer = window.innerWidth > 1024 // 1024px = Responsive level laptop
-        },
-        editTimer (item) {
-            this.editedIndex = this.timers.indexOf(item)
-            this.editedTimer = Object.assign({}, item)
-            this.dialog = true
-        },
-
-        deleteItem (item) {
-            const index = this.timers.indexOf(item)
-            confirm('Are you sure you want to delete this item?') && this.timers.splice(index, 1)
-        },
-
-        close () {
-            this.dialog = false
-            setTimeout(() => {
-                this.editedTimer = Object.assign({}, this.defaultTimer)
-                this.editedIndex = -1
-            }, 300)
-        },
-
-        save () {
-            if (this.editedIndex > -1) {
-                Object.assign(this.timers[this.editedIndex], this.editedTimer)
-            } else {
-                this.timers.push(this.editedTimer)
-            }
-            this.timer()
-            this.close()
-        },
-        timer: function() {
-            this.send({'type': 'timers', 'timers': this.timers})
-        },
-        login: function(password) {
-			this.send({'type': 'login', 'password': password})
-            this.password = ''
-        },
-        logout: function() {
-            this.sockets = {}
-            this.timers = []
-            vm.auth = false
-            document.cookie = "auth=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-            this.send({'type': 'logout'})
+  methods: {
+    send: function (raw) {
+      ws.send(JSON.stringify(raw))
+    },
+    power: function (id, action) {
+      this.socketWithID(id).status = action
+      /*this.sockets.forEach(function (socket) {
+        if (socket.id === id) {
+          socket.status = action
         }
-	},
-    computed: {
-        formTitle () {
-            return this.editedIndex === -1 ? 'New Timer' : 'Edit Timer'
-        }
+      })*/
+      // this.sockets[id].status = action
+      var raw = {
+        'type': 'sockets',
+        'sockets': this.sockets
+      }
+      this.send(raw)
+    },
+    setView: function (view) {
+      this.view = view
+      this.drawer = window.innerWidth > 1024 // 1024px = Responsive level laptop
+    },
+    editTimer (item) {
+      this.editedIndex = this.timers.indexOf(item)
+      this.editedTimer = Object.assign({}, item)
+      this.dialog = true
     },
 
-    watch: {
-        dialog (val) {
-            val || this.close()
+    deleteItem (item) {
+      const index = this.timers.indexOf(item)
+      confirm('Are you sure you want to delete this item?') && this.timers.splice(index, 1)
+    },
+
+    close () {
+      this.dialog = false
+      setTimeout(() => {
+        this.editedTimer = Object.assign({}, this.defaultTimer)
+        this.editedIndex = -1
+      }, 300)
+    },
+
+    save () {
+      if (this.editedIndex > -1) {
+        Object.assign(this.timers[this.editedIndex], this.editedTimer)
+      } else {
+        this.timers.push(this.editedTimer)
+      }
+      this.timer()
+      this.close()
+    },
+    timer: function () {
+      this.send({
+        'type': 'timers',
+        'timers': this.timers
+      })
+    },
+    login: function (password) {
+      this.send({
+        'type': 'login',
+        'password': password
+      })
+      this.password = ''
+    },
+    logout: function () {
+      this.sockets = []
+      this.timers = []
+      vm.auth = false
+      document.cookie = 'auth=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
+      this.send({
+        'type': 'logout'
+      })
+    },
+    socketWithID: function (id) {
+      var res
+      this.sockets.some(function (item) {
+        if (item.id === id) {
+          res = item
+          return true
         }
+      })
+      return res
     }
+  },
+  computed: {
+    formTitle () {
+      return this.editedIndex === -1 ? 'New Timer' : 'Edit Timer'
+    }
+  },
+
+  watch: {
+    dialog (val) {
+      val || this.close()
+    }
+  }
 })
 
 // Create WebSocket connection.
-if (window.location.protocol === "https:") {
-    var protocol = "wss:"
+if (window.location.protocol === 'https:') {
+  var protocol = 'wss:'
 } else {
-    var protocol = "ws:"
+  var protocol = 'ws:'
 }
 
 var ws = connectWebsocket(protocol + '//' + window.location.hostname + '/websocket/')
 
-function connectWebsocket(url) {
+function connectWebsocket (url) {
+  var result = new WebSocket(url)
 
-    var result = new WebSocket(url)
+  // Connection opened
+  result.onopen = function (event) {
+    console.log('WebSocket: connected')
+    vm.connected = true
+    vm.auth = false
+    vm.login(getCookie('auth'))
+  }
 
-    // Connection opened
-    result.onopen = function (event) {
-    	console.log('WebSocket: connected')
-    	vm.connected = true
-    	vm.auth = false
-        vm.login(getCookie('auth'))
-    }
+  // Listen for messages
+  result.onmessage = function (event) {
+    incoming(event.data)
+  }
 
-    // Listen for messages
-    result.onmessage = function (event) {
-    	incoming(event.data)
-    }
+  result.onerror = (e) => {}
 
-    result.onerror = (e) => {
-    }
+  result.onclose = function (e) {
+    console.log('WebSocket: disconnected')
+    vm.connected = false
+    setTimeout(() => {
+      ws = connectWebsocket(url)
+    }, 1000)
+  }
 
-    result.onclose = function (e) {
-    	console.log('WebSocket: disconnected')
-    	vm.connected = false
-    	setTimeout(() => {
-    		ws = connectWebsocket(url)
-    	}, 1000)
-    }
-
-    return result
+  return result
 }
 
-function incoming(input) {
-	var data
-	try {
-		data = JSON.parse(input);
-	} catch(e) {
-		alert(e)
-	}
-	switch(data.type) {
-		case 'sockets':
-			vm.sockets = data.sockets
-			break
-        case 'timers':
-            vm.timers = data.timers
-            break
-        case 'auth':
-            vm.auth = data.auth
-            if (vm.auth) {
-                setCookie('auth', data.cookie, 3000)
-            }
-            break
-	}
-	console.log('received: ' + data.type)
+function incoming (input) {
+  var data
+  try {
+    data = JSON.parse(input)
+  } catch (e) {
+    alert(e)
+  }
+  switch (data.type) {
+    case 'sockets':
+      vm.sockets = data.sockets
+      break
+    case 'timers':
+      vm.timers = data.timers
+      break
+    case 'auth':
+      vm.auth = data.auth
+      if (vm.auth) {
+        setCookie('auth', data.cookie, 3000)
+      }
+      break
+  }
+  console.log('received: ' + data.type)
 }
 
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+function setCookie (cname, cvalue, exdays) {
+  var d = new Date()
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000))
+  var expires = 'expires=' + d.toUTCString()
+  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/'
 }
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+function getCookie (cname) {
+  var name = cname + '='
+  var ca = document.cookie.split(';')
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i]
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1)
     }
-    return "";
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length)
+    }
+  }
+  return ''
 }
--- server.js
+++ server.js
@@ -5,266 +5,269 @@ const schedule = require('node-schedule')
 const SunCalc = require('suncalc')
 const CONFIG = require('./config.json')
 const mysqlCon = {
-    host     : CONFIG.db.host,
-    user     : CONFIG.db.user,
-    password : CONFIG.db.password,
-    database : CONFIG.db.name
+  host: CONFIG.db.host,
+  user: CONFIG.db.user,
+  password: CONFIG.db.password,
+  database: CONFIG.db.name
 }
 
 var db = connectMysql(mysqlCon)
 
-const wss = new WebSocket.Server({ port: CONFIG.wsPort })
+const wss = new WebSocket.Server({
+  port: CONFIG.wsPort
+})
 
 var sockets = []
 var timers
 var jobs = []
 
 db.query('SELECT * FROM sockets', function (error, results, fields) {
-	if (error) throw error
-    results.forEach( function(result) {
-        sockets[result.id] = result
-    })
-
-});
-
-db.query('SELECT * FROM timers', function (error, results, fields) {
-	if (error) throw error
-
-	timers = results
-
-    updateJobs()
+  if (error) throw error
+  /*results.forEach(function (result) {
+    sockets[result.id] = result
+  })*/
+  sockets = results
 })
 
-wss.on('connection', function connection(ws) {
+db.query('SELECT * FROM timers', function (error, results, fields) {
+  if (error) throw error
 
-	ws.on('error', () => console.log('errored'))
+  timers = results
 
-	initConnection(ws)
+  updateJobs()
+})
 
-	ws.on('message', (msg) => {
-		incoming(ws, msg)
-	})
+wss.on('connection', function connection (ws) {
+  ws.on('error', () => console.log('errored'))
 
-	ws.on('pong', () => {
-      		ws.isAlive = true
-    })
+  initConnection(ws)
+
+  ws.on('message', (msg) => {
+    incoming(ws, msg)
+  })
+
+  ws.on('pong', () => {
+    ws.isAlive = true
+  })
 })
 
 // WebSocket keep alive pings
 setInterval(() => {
-	wss.clients.forEach(function each (ws) {
-		if (ws.isAlive === false) return ws.terminate()
+  wss.clients.forEach(function each (ws) {
+    if (ws.isAlive === false) return ws.terminate()
 
-		ws.isAlive = false
-		ws.ping(() => {})
-	})
+    ws.isAlive = false
+    ws.ping(() => {})
+  })
 }, 30000)
 
 // refresh Jobs regularly because sunTimes change
-var refresh = schedule.scheduleJob(CONFIG.jobRefresh, function(){
-    updateJobs()
+schedule.scheduleJob(CONFIG.jobRefresh, function () {
+  updateJobs()
 })
 
-function connectMysql(connection) {
+function connectMysql (connection) {
+  var result = mysql.createConnection(connection)
 
-    var result = mysql.createConnection(connection)
-
-    result.on('error', function(err) {
-    	if(err.code == 'PROTOCOL_CONNECTION_LOST') {
-    		db = connectMysql(connection)
-    	}
-    	console.log(err.code) // 'ER_BAD_DB_ERROR'
-    })
-
-    return result
-}
-
-function initConnection(ws) {
-	ws.isAlive = true
-    ws.auth = false
-}
-
-function incoming(ws, message) {
-
-	// temp var to find changes
-	var data
-
-	try {
-		// hopefully message is always as expected :p
-		data = JSON.parse(message)
-	} catch(e) {
-		console.log(e)
-	}
-
-    if (ws.auth) {
-    	switch (data.type) {
-    		case 'sockets':
-    			updateSockets(data.sockets)
-                var raw = {
-                    'type': 'sockets',
-                    'sockets': sockets
-                }
-                broadcastOthers(ws, raw)
-    			break
-            case 'timers':
-                console.log(data.timers);
-                updateTimers(data.timers)
-                updateJobs()
-                var raw = {
-                    'type': 'timers',
-                    'timers': timers
-                }
-                broadcastOthers(ws, raw)
-                break
-            case 'logout':
-                authConnection(ws, false)
-                break
-        }
+  result.on('error', function (err) {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      db = connectMysql(connection)
     }
-    switch (data.type) {
-        case 'login':
-            if(data.password === CONFIG.auth.password || data.password === CONFIG.auth.cookie) {
-                authConnection(ws, true)
-                initialData(ws)
-            }
-            break
-	}
+    console.log(err.code) // 'ER_BAD_DB_ERROR'
+  })
 
-    console.log('received: ' + data.type)
+  return result
+}
+
+function initConnection (ws) {
+  ws.isAlive = true
+  ws.auth = false
+}
+
+function incoming (ws, message) {
+  // temp var to find changes
+  var data
+
+  try {
+    // hopefully message is always as expected :p
+    data = JSON.parse(message)
+  } catch (e) {
+    console.log(e)
+  }
+
+  if (ws.auth) {
+    switch (data.type) {
+      case 'sockets':
+        updateSockets(data.sockets)
+        var raw = {
+          'type': 'sockets',
+          'sockets': sockets
+        }
+        broadcastOthers(ws, raw)
+        break
+      case 'timers':
+        console.log(data.timers)
+        updateTimers(data.timers)
+        updateJobs()
+        var raw = {
+          'type': 'timers',
+          'timers': timers
+        }
+        broadcastOthers(ws, raw)
+        break
+      case 'logout':
+        authConnection(ws, false)
+        break
+    }
+  }
+  switch (data.type) {
+    case 'login':
+      if (data.password === CONFIG.auth.password || data.password === CONFIG.auth.cookie) {
+        authConnection(ws, true)
+        initialData(ws)
+      }
+      break
+  }
+
+  console.log('received: ' + data.type)
 }
 
 function sendConnection (ws, data) {
-	if (ws.readyState === ws.OPEN) {
-		ws.send(JSON.stringify(data))
-	}
+  if (ws.readyState === ws.OPEN) {
+    ws.send(JSON.stringify(data))
+  }
 }
 
-function authConnection(ws, state) {
-    ws.auth = state
-    if(ws.auth) {
-        sendConnection(ws, {
-            'type': 'auth',
-            'auth': ws.auth,
-            'cookie': CONFIG.auth.cookie
-        })
-    }
-
-}
-
-function updateSockets(data){
-	var old = sockets
-	sockets = data
-    if (sockets.length == old.length) { // test not really good enough
-
-        sockets.forEach( function(item, i) {
-            // sockets[i] === item
-            //if(sockets[i] != old[i]) {
-            if(item.status != old[i].status) { // okay for now, only changing status anyway
-
-				db.query('UPDATE sockets SET status = ? WHERE id = ?', [item.status, item.id], function (error, results, fields) {
-					if (error) throw error
-				})
-
-				if(sockets[i].status != old[i].status) {
-					cmd.run("sudo ./codesendRoot " + (item.status == 1 ? parseInt(item.code_on, 10) : parseInt(item.code_off, 10)) + " " + parseInt(item.protocol, 10))
-				}
-			}
-		})
-    }
-}
-
-function broadcastOthers(ws, raw) {
-    wss.clients.forEach( function(client) {
-        if(client !== ws && client.readyState === WebSocket.OPEN){
-            sendConnection(client, raw)
-        }
-    })
-}
-
-function broadcastAll() {
-    wss.clients.forEach( function(client) {
-        if (client.readyState === WebSocket.OPEN) {
-            sendConnection(client, {
-                'type': 'sockets',
-                'sockets': sockets
-            })
-        }
-    })
-}
-
-function updateJobs() {
-    clearJobs()
-    var sunTimes
-    timers.forEach( function(timer, i) {
-
-        // set sunTimes once
-        if(timer.mode != 'time' && sunTimes == null){
-            sunTimes = SunCalc.getTimes(new Date(), CONFIG.loc.lat, CONFIG.loc.lng)
-        }
-
-        jobs[i] = schedule.scheduleJob(jobTime(timer, sunTimes), function(){
-            if(sockets[timer.socket_id].status != timer.action) {
-
-                var data = JSON.parse(JSON.stringify(sockets)) // deep copy of sockets
-                data[timer.socket_id].status = timer.action
-
-                updateSockets(data)
-                broadcastAll()
-
-            }
-        })
-    })
-}
-
-function clearJobs(){
-    jobs.forEach( function(job) {
-        job.cancel()
-    })
-    jobs = []
-}
-
-function jobTime(timer, sunTimes) {
-
-    if (timer.mode == 'time') {
-        var result = timer.minute + ' ' + timer.hour + ' ' + timer.dom + ' ' + timer.month + ' ' + timer.dow
-    } else {
-        var offsetDate = addMinutes(sunTimes[timer.mode], timer.offset)
-        var result = offsetDate.getMinutes() + ' ' + offsetDate.getHours() + ' * * *'
-    }
-
-    return result
-}
-
-function updateTimers(data) {
-    data.forEach( function(timer, i) {
-        if(timer != timers[i]){
-            if(i >= timers.length){
-                db.query('INSERT INTO timers (socket_id, action, mode, offset, minute, hour, dom, month, dow) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [timer.socket_id, timer.action, timer.mode, timer.offset, imer.minute, timer.hour, timer.dom, timer.month, timer.dow, timer.id], function (error, results, fields) {
-                    if (error) throw error
-                })
-            } else {
-                db.query('UPDATE timers SET socket_id = ?, action = ?, mode = ?, offset = ?, minute = ?, hour = ?, dom = ?, month = ?, dow = ? WHERE id = ?', [timer.socket_id, timer.action, timer.mode, timer.offset, timer.minute, timer.hour, timer.dom, timer.month, timer.dow, timer.id], function (error, results, fields) {
-                    if (error) throw error
-                })
-            }
-        }
-    })
-    timers = data
-}
-
-function initialData(ws) {
-    // initial data for client
+function authConnection (ws, state) {
+  ws.auth = state
+  if (ws.auth) {
     sendConnection(ws, {
+      'type': 'auth',
+      'auth': ws.auth,
+      'cookie': CONFIG.auth.cookie
+    })
+  }
+}
+
+function updateSockets (data) {
+  var old = sockets
+  sockets = data
+  if (sockets.length === old.length) { // test not really good enough
+    sockets.forEach(function (item, i) {
+      // sockets[i] === item
+      // if(sockets[i] != old[i]) {
+      if (item.status !== old[i].status) { // okay for now, only changing status anyway
+        db.query('UPDATE sockets SET status = ? WHERE id = ?', [item.status, item.id], function (error, results, fields) {
+          if (error) throw error
+        })
+
+        if (sockets[i].status !== old[i].status) {
+          cmd.run('sudo ./codesendRoot ' + (item.status === 1 ? parseInt(item.code_on, 10) : parseInt(item.code_off, 10)) + ' ' + parseInt(item.protocol, 10))
+        }
+      }
+    })
+  }
+}
+
+function broadcastOthers (ws, raw) {
+  wss.clients.forEach(function (client) {
+    if (client !== ws && client.readyState === WebSocket.OPEN) {
+      sendConnection(client, raw)
+    }
+  })
+}
+
+function broadcastAll () {
+  wss.clients.forEach(function (client) {
+    if (client.readyState === WebSocket.OPEN) {
+      sendConnection(client, {
         'type': 'sockets',
         'sockets': sockets
-    })
-
-    sendConnection(ws, {
-        'type': 'timers',
-        'timers': timers
-    })
+      })
+    }
+  })
 }
 
-function addMinutes(date, minutes) {
-   return new Date(date.getTime() + minutes * 60000);
+function updateJobs () {
+  clearJobs()
+  var sunTimes
+  timers.forEach(function (timer, i) {
+    // set sunTimes once
+    if (timer.mode !== 'time' && sunTimes == null) {
+      sunTimes = SunCalc.getTimes(new Date(), CONFIG.loc.lat, CONFIG.loc.lng)
+    }
+
+    jobs[i] = schedule.scheduleJob(jobTime(timer, sunTimes), function () {
+      if (objectWithID(sockets, timer.socket_id).status !== timer.action) {
+        var data = JSON.parse(JSON.stringify(sockets)) // deep copy of sockets
+        objectWithID(data, timer.socket_id).status = timer.action
+
+        updateSockets(data)
+        broadcastAll()
+      }
+    })
+  })
+}
+
+function clearJobs () {
+  jobs.forEach(function (job) {
+    job.cancel()
+  })
+  jobs = []
+}
+
+function jobTime (timer, sunTimes) {
+  if (timer.mode === 'time') {
+    var result = timer.minute + ' ' + timer.hour + ' ' + timer.dom + ' ' + timer.month + ' ' + timer.dow
+  } else {
+    var offsetDate = addMinutes(sunTimes[timer.mode], timer.offset)
+    var result = offsetDate.getMinutes() + ' ' + offsetDate.getHours() + ' * * *'
+  }
+
+  return result
+}
+
+function updateTimers (data) {
+  data.forEach(function (timer, i) {
+    if (timer !== timers[i]) {
+      if (i >= timers.length) {
+        db.query('INSERT INTO timers (socket_id, action, mode, offset, minute, hour, dom, month, dow) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [timer.socket_id, timer.action, timer.mode, timer.offset, timer.minute, timer.hour, timer.dom, timer.month, timer.dow], function (error, results, fields) {
+          if (error) throw error
+        })
+      } else {
+        db.query('UPDATE timers SET socket_id = ?, action = ?, mode = ?, offset = ?, minute = ?, hour = ?, dom = ?, month = ?, dow = ? WHERE id = ?', [timer.socket_id, timer.action, timer.mode, timer.offset, timer.minute, timer.hour, timer.dom, timer.month, timer.dow, timer.id], function (error, results, fields) {
+          if (error) throw error
+        })
+      }
+    }
+  })
+  timers = data
+}
+
+function initialData (ws) {
+  // initial data for client
+  sendConnection(ws, {
+    'type': 'sockets',
+    'sockets': sockets
+  })
+
+  sendConnection(ws, {
+    'type': 'timers',
+    'timers': timers
+  })
+}
+
+function addMinutes (date, minutes) {
+  return new Date(date.getTime() + minutes * 60000)
+}
+
+function objectWithID (array, id) {
+  var res
+  array.some(function (item) {
+    if (item.id === id) {
+      res = item
+      return true
+    }
+  })
+  return res
 }
